6bf012fa096faf47f79e3f11186d7d067c5ec423
Bryan BOHEC <bohebr1503@iutparis.local>
2022-10-06 16:08:56 +0200

    Changement index et news

--- news.js
+++ news.js
@@ -2,6 +2,7 @@ const news =
 [
     {
         "title": "La Classe des 18 ans reprend du service !",
+        "type": "",
         "imagesrc": "",
         "description": "Si depuis février 2020, la Classe des 18 ans - projet initié par le Pôle Jeunesse de Mont de Marsan Agglo - est impactée par le contexte sanitaire, la promotion 2004 a décidé de relancer les activités pour permettre aux jeunes du territoire de se retrouver autour de moments festifs et conviviaux."
     },
@@ -9,53 +10,67 @@ const news =
     {
         "title": "La Classe des 18 ans reprend du service !",
         "imagesrc": "",
+        "type": "",
         "description": "Si depuis février 2020, la Classe des 18 ans - projet initié par le Pôle Jeunesse de Mont de Marsan Agglo - est impactée par le contexte sanitaire, la promotion 2004 a décidé de relancer les activités pour permettre aux jeunes du territoire de se retrouver autour de moments festifs et conviviaux."
     },
     
     {
         "title": "Adaptation des services municipaux et communautaires aux nouvelles mesures sanitaires",
+        "type": "",
         "imagesrc": "",
         "description": "En raison de la dégradation du contexte sanitaire, de nouvelles mesures sanitaires sont mises en place sur le territoire jusqu'au 3 février.\nSi les services municipaux et communautaires restent ouverts au grand public, certains adaptent leurs horaires."
     },
 
     {
         "title": "La Classe des 18 ans reprend du service !",
+        "type": "",
         "imagesrc": "",
         "description": "Si depuis février 2020, la Classe des 18 ans - projet initié par le Pôle Jeunesse de Mont de Marsan Agglo - est impactée par le contexte sanitaire, la promotion 2004 a décidé de relancer les activités pour permettre aux jeunes du territoire de se retrouver autour de moments festifs et conviviaux."
     },
 
     {
         "title": "La Classe des 18 ans reprend du service !",
+        "type": "",
         "imagesrc": "",
         "description": "Si depuis février 2020, la Classe des 18 ans - projet initié par le Pôle Jeunesse de Mont de Marsan Agglo - est impactée par le contexte sanitaire, la promotion 2004 a décidé de relancer les activités pour permettre aux jeunes du territoire de se retrouver autour de moments festifs et conviviaux."
     }
     
 ];
 
-function loadNews() {
-    const regex = /(?:\r\n|\r|\n)/g;
-    var page = document.getElementsByClassName('newsList');
-    page = page[0];
-    var title, imagesrc, description;
+const MAX_CH = 400;
+
+$(document).ready(function() {
+    loadCards();
+})
+
+function loadCards() {
+    var title, type, imagesrc, description;
     news.forEach(element => {
-        
+
         title = element.title;
-        title.replaceAll(regex,'<br/>hdhgfd');
-        console.log(title);
         description = element.description;
-        description.replaceAll(regex,'<br/>hgfdhgfd');
+        type = (element.type) == "" ? "Communauté" : element.type;
+        imagesrc = (element.imagesrc) == "" ? "./images/defaultNews.jpg" : element.imagesrc;
+        $(".container").append(`
 
-        imagesrc = (element.imagesrc) == "" ? "./images/defaultNewsImg.jpg" : element.imagesrc;
+            <a href="#" class="newsCard">
+              <div class="cardHeading">
+                <img src="${imagesrc}">
+              </div>
+              <div class="cardBody">
+                <span class="cardType">${type}</span>
+                <h4 class="cardTitle">${title}</h4>
+                <p class="cardContent">${description}</p>
+              </div>
+            </a44>`)
+    })
 
-        page.innerHTML += `
-        <div class="newsBox">
-            <img src="${imagesrc}">
-            <div class="newsContent">
-                <h2 class="title">${title}</h2>
-                <br>
-                <p class="desc">${description}</p>
-            </div>
-        </div>
-        `; 
-    });
+    $(".cardContent").each(function(i, obj) {
+        console.log("oe");
+        let str = $(this).text();
+
+        if($(this).text().length > MAX_CH) {
+            $(this).text((str.substring(0, MAX_CH).trim()) + "...");
+        }
+    })
 }
